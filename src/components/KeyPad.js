d4fc13df353fea1dc5a6aafb499402d00d77afbb
Andrés Nuñez <andresanunez98@gmail.com>
2021-08-11 15:50:56 -0400

    finished buttons in keypay component

--- src/components/KeyPad.js
+++ src/components/KeyPad.js
@@ -4,10 +4,30 @@ class KeyPad extends Component {
   render() {
     return (
       <div className="button">
-        <button name="("> ( </button>
-        <button name="CE"> CE </button>
-        <button name=")"> ) </button>
-        <button name="C"> C </button>
+        <button name="(">(</button>
+        <button name="CE">CE</button>
+        <button name=")">)</button>
+        <button name="C">C</button>
+        <br></br>
+        <button name="1">1</button>
+        <button name="2">2</button>
+        <button name="3">3</button>
+        <button name="+">+</button>
+        <br></br>
+        <button name="4">4</button>
+        <button name="5">5</button>
+        <button name="6">6</button>
+        <button name="-">-</button>
+        <br></br>
+        <button name="7">7</button>
+        <button name="8">8</button>
+        <button name="9">9</button>
+        <button name="*">*</button>
+        <br></br>
+        <button name=".">.</button>
+        <button name="0">0</button>
+        <button name="=">=</button>
+        <button name="/">/</button>
         <br></br>
       </div>
     );
