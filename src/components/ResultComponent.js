915b05a16f1063ee971fe70b45ae4645afdcf09e
Andrés Nuñez <andresanunez98@gmail.com>
2021-08-11 11:56:15 -0400

    built out ResultComponent

---
+++ src/components/ResultComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class ResultComponent extends Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.result}</p>
+      </div>
+    );
+  }
+}
